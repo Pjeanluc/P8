88dfa4982f178c21055e95fb27fc5dd1d984a093
Pjeanluc <jeanluc.protois@axa.fr>
2020-10-20 16:03:11 +0200

    (feature): finalisation test UT et IT

--- src/test/java/tourGuide/TestPerformance.java
+++ src/test/java/tourGuide/TestPerformance.java
@@ -2,19 +2,18 @@ package tourGuide;
 
 import static org.junit.Assert.assertTrue;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.UUID;
-import java.util.concurrent.TimeUnit;
+import java.util.*;
+import java.util.concurrent.*;
 
 import org.apache.commons.lang3.time.StopWatch;
 import org.junit.Ignore;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import gpsUtil.GpsUtil;
 import gpsUtil.location.Attraction;
 import gpsUtil.location.VisitedLocation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import rewardCentral.RewardCentral;
 import tourGuide.helper.InternalTestHelper;
 import tourGuide.service.RewardsService;
@@ -23,7 +22,7 @@ import tourGuide.user.User;
 import tourGuide.user.UserReward;
 
 public class TestPerformance {
-	
+
 	/*
 	 * A note on performance improvements:
 	 *     
@@ -43,24 +42,24 @@ public class TestPerformance {
      *     highVolumeGetRewards: 100,000 users within 20 minutes:
 	 *          assertTrue(TimeUnit.MINUTES.toSeconds(20) >= TimeUnit.MILLISECONDS.toSeconds(stopWatch.getTime()));
 	 */
-	
-	@Ignore
+
 	@Test
-	public void highVolumeTrackLocation() {
+	public void highVolumeTrackLocation() throws InterruptedException {
+		Locale.setDefault(Locale.US);
 		GpsUtil gpsUtil = new GpsUtil();
 		RewardsService rewardsService = new RewardsService(gpsUtil, new RewardCentral());
 		// Users should be incremented up to 100,000, and test finishes within 15 minutes
-		InternalTestHelper.setInternalUserNumber(100);
+		InternalTestHelper.setInternalUserNumber(1);
 		TourGuideService tourGuideService = new TourGuideService(gpsUtil, rewardsService);
 
-		List<User> allUsers = new ArrayList<>();
-		allUsers = tourGuideService.getAllUsers();
+		List<User> allUsers = tourGuideService.getAllUsers();
 		
 	    StopWatch stopWatch = new StopWatch();
 		stopWatch.start();
-		for(User user : allUsers) {
-			tourGuideService.trackUserLocation(user);
-		}
+		tourGuideService.trackListOfUserLocation(allUsers);
+		//for(User user : allUsers) {
+		//	tourGuideService.trackUserLocation(user);
+		//}
 		stopWatch.stop();
 		tourGuideService.tracker.stopTracking();
 
@@ -68,14 +67,15 @@ public class TestPerformance {
 		assertTrue(TimeUnit.MINUTES.toSeconds(15) >= TimeUnit.MILLISECONDS.toSeconds(stopWatch.getTime()));
 	}
 	
-	@Ignore
+
 	@Test
-	public void highVolumeGetRewards() {
+	public void highVolumeGetRewards() throws InterruptedException {
+		Locale.setDefault(Locale.US);
 		GpsUtil gpsUtil = new GpsUtil();
 		RewardsService rewardsService = new RewardsService(gpsUtil, new RewardCentral());
 
 		// Users should be incremented up to 100,000, and test finishes within 20 minutes
-		InternalTestHelper.setInternalUserNumber(100);
+		InternalTestHelper.setInternalUserNumber(1);
 		StopWatch stopWatch = new StopWatch();
 		stopWatch.start();
 		TourGuideService tourGuideService = new TourGuideService(gpsUtil, rewardsService);
@@ -83,13 +83,24 @@ public class TestPerformance {
 	    Attraction attraction = gpsUtil.getAttractions().get(0);
 		List<User> allUsers = new ArrayList<>();
 		allUsers = tourGuideService.getAllUsers();
-		allUsers.forEach(u -> u.addToVisitedLocations(new VisitedLocation(u.getUserId(), attraction, new Date())));
-	     
-	    allUsers.forEach(u -> rewardsService.calculateRewards(u));
-	    
+
+		ExecutorService executorService = Executors.newFixedThreadPool(2000);
+		//ExecutorService executorService = new ThreadPoolExecutor(1, 1, 1000,
+		//		TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>());
+
 		for(User user : allUsers) {
-			assertTrue(user.getUserRewards().size() > 0);
+
+			Runnable runnableTask = () -> {
+				user.addToVisitedLocations(new VisitedLocation(user.getUserId(), attraction, new Date()));
+				rewardsService.calculateRewards(user);
+				assertTrue(user.getUserRewards().size() > 0);
+			};
+
+		executorService.execute(runnableTask);
 		}
+		executorService.shutdown();
+		executorService.awaitTermination(25, TimeUnit.MINUTES);
+
 		stopWatch.stop();
 		tourGuideService.tracker.stopTracking();
 
